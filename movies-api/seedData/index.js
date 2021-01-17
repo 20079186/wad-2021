c3c057b5ed4c689b867c58d9fbcbcfdecee2637a
Amie Abbotts <20079186@mail.wit.ie>
2021-01-17 09:52:22 +0000

    Seed data files - not complete

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,7 +1,14 @@
 import userModel from '../api/users/userModel.js';
 import movieModel from '../api/movies/movieModel';
+import latestMovieModel from '../api/latestMovie/latestMovieModel';
+import popularMovieModel from '../api/popularMovies/popularMoviesModel';
+import upcomingMovieModel from '../api/upcomingMovies/upcomingMoviesModel';
+import nowPlayingMovieModel from '../api/nowPlayingMovies/nowPlayingMoviesModel';
 import {movies} from './movies.js';
-
+import {upcoming} from './upcomingMovies';
+import {nowPlaying} from './nowPlayingMovies';
+import {latest} from './latestMovie';
+import {popular} from './popularMovies';
 
 const users = [
   {
@@ -37,4 +44,47 @@ export async function loadUsers() {
     }
   }
 
- 
+  export async function loadUpcomingMovies() {
+    console.log('load seed data');
+    console.log(upcoming.length);
+    try {
+      await upcomingMovieModel.deleteMany();
+      await upcomingMovieModel.collection.insertMany(upcoming);
+      console.info(`${upcoming.length} Movies were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load movie Data: ${err}`);
+    }
+  }
+  export async function loadNowPlayingMovies() {
+    console.log('load seed data');
+    console.log(nowPlaying.length);
+    try {
+      await nowPlayingMovieModel.deleteMany();
+      await nowPlayingMovieModel.collection.insertMany(nowPlaying);
+      console.info(`${nowPlaying.length} Movies were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load movie Data: ${err}`);
+    }
+  }
+  export async function loadPopularMovies() {
+    console.log('load seed data');
+    console.log(popular.length);
+    try {
+      await popularMovieModel.deleteMany();
+      await popularMovieModel.collection.insertMany(upcoming);
+      console.info(`${popular.length} Movies were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load movie Data: ${err}`);
+    }
+  }
+  export async function loadLatestMovie() {
+    console.log('load seed data');
+    console.log(latest.length);
+    try {
+      await latestMovieModel.deleteMany();
+      await latestMovieModel.collection.insertMany(upcoming);
+      console.info(`${latest.length} Movies were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load movie Data: ${err}`);
+    }
+  }
